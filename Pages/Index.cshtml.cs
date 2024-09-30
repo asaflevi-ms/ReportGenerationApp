e3d416a297384ba7b7379317981f01bfd5fea878
Asaf Levi <asaflevi@microsoft.com>
2024-09-30 12:34:19 +0300

    add spinner

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -33,7 +34,11 @@ namespace ReportGenerationApp.Pages
 
             using var stream = JsonFile.OpenReadStream();
             using var reader = new StreamReader(stream);
-            InputJson = await reader.ReadToEndAsync();
+            var jsonString = await reader.ReadToEndAsync();
+
+            // Format the JSON
+            var jsonDocument = JsonDocument.Parse(jsonString);
+            InputJson = JsonSerializer.Serialize(jsonDocument, new JsonSerializerOptions { WriteIndented = true });
 
             return Page();
         }
@@ -46,20 +51,31 @@ namespace ReportGenerationApp.Pages
                 return Page();
             }
 
-            using var content = new StringContent(InputJson, System.Text.Encoding.UTF8, "application/json");
+            try
+            {
+                using var content = new StringContent(InputJson, System.Text.Encoding.UTF8, "application/json");
 
-            var client = _httpClientFactory.CreateClient();
+                var client = _httpClientFactory.CreateClient();
 
-            // Acquire the token using TokenService
-            var token = await _tokenService.GetTokenAsync(new[] { "https://hls-repgen.azure-test.net/.default" });
+                // Acquire the token using TokenService
+                var token = await _tokenService.GetTokenAsync(new[] { "https://hls-repgen.azure-test.net/.default" });
 
-            // Add the Bearer token to the request headers
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                // Add the Bearer token to the request headers
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
 
-            var response = await client.PostAsync("https://reportgeneration.test.workspace.mshapis.com/radiology/generate-draft-report", content);
+                var response = await client.PostAsync("https://reportgeneration.dev.workspace.mshapis.com/radiology/generate-draft-report", content);
 
-            ResponseJson = await response.Content.ReadAsStringAsync();
+                ResponseJson = await response.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                ResponseJson = $"Error: {ex.Message}";
+            }
 
+            // Format the JSON indenting
+            var jsonDocument = JsonDocument.Parse(ResponseJson);
+            ResponseJson = JsonSerializer.Serialize(jsonDocument, new JsonSerializerOptions { WriteIndented = true });
+            
             return Page();
         }
 
